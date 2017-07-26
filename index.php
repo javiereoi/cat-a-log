f2e13145cc2fd6363211843027e873d8a1e6bd55
Mónica Prados <monpragodev@gmail.com>
2017-07-26 13:12:35 +0200

    Crear index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Author : Mónica Prados Gómez
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cat-a-log</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
